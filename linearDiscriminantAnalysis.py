6a2db2093c5d2ad82722dec3179c09e385624f25
Ammar Rudani <aarudani7@gmail.com>
2019-09-24 20:45:45 -0400

    covariance function

--- linearDiscriminantAnalysis.py
+++ linearDiscriminantAnalysis.py
@@ -24,18 +24,31 @@ class linearDiscriminantAnalysis:
         print(probOf0) 
 
     
-    def split_data(self, X):
-        y = X[:,-1]
+    def mean(self, X):
+        y = X.iloc[:,-1]
         sum0 = 0
         sum1 = 0
         
 
-        for i in range(y):
+        for i in range(len(y.index)):
+            counter0 = 0
+            counter1 = 0
             if y[i] == 0:
-                w = X[i,:]
-                for j in range(w):
+                w = X.iloc[i,:]
+                for j in range(len(w.index)):
                     sum0 += w[j]
-                    
+                counter0 = counter0 + 1
+             
+            if y[i] == 1:
+                w = X.iloc[i,:]
+                for j in range(len(w.index)):
+                    sum1 += w[j]
+                counter1 = counter1 + 1    
+
+
+            tup1 = (sum0/counter0, sum1/counter1)
+            return tup1
+                
 
 
 
@@ -43,20 +56,21 @@ class linearDiscriminantAnalysis:
 
     
 
-    def calculate_covariance_matrix(X, Y=None):
-        if Y is None:
-            Y = X
+    def calculate_covariance_matrix(self, X):
+            Y = (X- X.mean(axis=0))
             n_samples = np.shape(X)[0]
-            covariance_matrix = (1/(n_samples-2)) * (X- X.mean(axis=0)).T.dot(Y-Y.mean(axis=0))
+            covariance_matrix = Y.T.dot(Y)
             return np.array(covariance_matrix, dtype=float)
     
 
     def fit(self, sample, y):
         sample_0 = sample[y == 0]
         sample_1 = sample[y == 1]
+        n_samples = np.shape(sample)[0]
 
         cVariance_0 = calculate_covariance_matrix(sample_0)
         cVariance_1 = calculate_covariance_matrix(sample_1)
 
         cVariance = cVariance_0 + cVariance_1
+        cVariance = cVariance/(n_samples - 2)
 
