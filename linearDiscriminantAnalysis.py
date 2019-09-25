2057f80bb4fdab0817f3f3d97183a4f9845e4876
LiamBlount <50602230+LiamBlount@users.noreply.github.com>
2019-09-25 15:03:51 -0400

    Update linearDiscriminantAnalysis.py

--- linearDiscriminantAnalysis.py
+++ linearDiscriminantAnalysis.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import pandas as pd
 import sys
 
 class linearDiscriminantAnalysis:
@@ -55,22 +56,50 @@ class linearDiscriminantAnalysis:
 
 
     
-
+    def predict(self,X,y,x0):
+        (CovI,u0,u1,P0,P1)=self.fit(X,y)
+        a=x0.dot(CovI).dot(u0)-0.5*u0.T.dot(CovI).dot(u0)+P0
+        a=a.values.astype(float)
+        a=a[0]
+        b=x0.dot(CovI).dot(u1)-0.5*u1.T.dot(CovI).dot(u1)+P1
+        b=b.values.astype(float)
+        b=b[0]
+        if (a>=b):
+            return 0
+        else:
+            return 1
+        
     def calculate_covariance_matrix(self, X):
-            Y = (X- X.mean(axis=0))
+            u=X.mean(axis=0)
+            #u=u.mean(axis=0)
+            Y = (X- u)
             n_samples = np.shape(X)[0]
             covariance_matrix = Y.T.dot(Y)
-            return np.array(covariance_matrix, dtype=float)
+            return covariance_matrix
     
 
     def fit(self, sample, y):
         sample_0 = sample[y == 0]
         sample_1 = sample[y == 1]
         n_samples = np.shape(sample)[0]
+        u0=sample_0.mean(axis=0)
+        
+        u1=sample_1.mean(axis=0)
 
-        cVariance_0 = calculate_covariance_matrix(sample_0)
-        cVariance_1 = calculate_covariance_matrix(sample_1)
+        cVariance_0 = self.calculate_covariance_matrix(sample_0)
+        cVariance_1 = self.calculate_covariance_matrix(sample_1)
 
         cVariance = cVariance_0 + cVariance_1
         cVariance = cVariance/(n_samples - 2)
-
+    
+        cVarianceInverse = pd.DataFrame(np.linalg.pinv(cVariance.values.astype(float)), cVariance.columns, cVariance.index)
+        
+        
+        
+        n_sample0=np.shape(sample_0)[0]
+        n_sample1=np.shape(sample_1)[0]
+        
+        P1=math.log(n_sample1/n_samples)
+        P0=math.log(n_sample0/n_samples)
+        return (cVarianceInverse,u0,u1,P0,P1)
+        
